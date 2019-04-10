398794fe6d72a98e5dede7580dee14571f846fe1
xiaoyh <1273859438@qq.com>
2019-04-10 17:38:25 +0800

    新增nacos配置中心

---
+++ service-nacos/src/main/java/cn/xiaoyh/springcloud/nacos/NacosAppliaction.java
@@ -0,0 +1,11 @@
+package cn.xiaoyh.springcloud.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosAppliaction {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosAppliaction.class, args);
+    }
+}
